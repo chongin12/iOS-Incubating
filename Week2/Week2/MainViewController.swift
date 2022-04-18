de2a7b6246c553c0bdfbb908f87e79b2d5d3626c
정종인 <chongin12@naver.com>
2022-04-18 22:04:04 +0900

    Week2 : organize structure

---
+++ Week2/Week2/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week2
+//
+//  Created by 정종인 on 2022/04/18.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
