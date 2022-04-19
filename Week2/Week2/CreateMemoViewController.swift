71843a95c13f16b71305fa6675b9104c7ceec3d0
정종인 <chongin12@naver.com>
2022-04-19 20:25:13 +0900

    Week 2 : create memo

--- Week2/Week2/CreateMemoViewController.swift
+++ Week2/Week2/CreateMemoViewController.swift
@@ -8,22 +8,91 @@
 import UIKit
 
 class CreateMemoViewController: UIViewController {
+    lazy var baseView: UIView = {
+        let baseView = UIView()
+        
+        baseView.backgroundColor = .systemBackground
+        
+        return baseView
+    }()
+    
+    lazy var titleTextField: UITextField = {
+        let textField = UITextField()
+        
+        textField.placeholder = "제목 입력"
+        textField.borderStyle = .roundedRect
+        
+        return textField
+    }()
+    
+    lazy var bodyTextView: UITextView = {
+        let textView = UITextView()
+        
+        textView.backgroundColor = .systemGray6
+        
+        return textView
+    }()
+    
+    lazy var doneBarButtonItem: UIBarButtonItem = {
+        let barButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(finishCreatingMemo(_:)))
+        
+        return barButtonItem
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setLayouts()
+        setNavigationItem()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setLayouts() {
+        setViewHierarchy()
+        setConstraints()
     }
-    */
-
+    
+    private func setNavigationItem() {
+        self.navigationItem.title = "메모 작성"
+        self.navigationItem.rightBarButtonItem = doneBarButtonItem
+    }
+    
+    private func setViewHierarchy() {
+        self.view.addSubview(baseView)
+        baseView.addSubview(titleTextField)
+        baseView.addSubview(bodyTextView)
+    }
+    
+    private func setConstraints() {
+        baseView.snp.makeConstraints {
+            $0.top.bottom.leading.trailing.equalToSuperview()
+        }
+        
+        titleTextField.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(baseView.safeAreaLayoutGuide).inset(Size.Margin.small)
+            $0.height.equalTo(Size.Area.large)
+        }
+        
+        bodyTextView.snp.makeConstraints {
+            $0.top.equalTo(titleTextField.snp.bottom).offset(Size.Margin.small)
+            $0.leading.trailing.bottom.equalTo(baseView.safeAreaLayoutGuide).inset(Size.Margin.small)
+        }
+    }
+    
+    @objc
+    private func finishCreatingMemo(_ sender: UIBarButtonItem) {
+        let newMemo: Memo = Memo(title: titleTextField.text ?? "", body: bodyTextView.text ?? "")
+        
+        StoredMemo.shared.memos.append(newMemo)
+        
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    struct Size {
+        struct Area {
+            static let large: CGFloat = 30
+        }
+        struct Margin {
+            static let small: CGFloat = 5
+        }
+    }
 }
